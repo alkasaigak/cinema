f661bcf6cdc838e6580757142ac27a2d7f3755c3
alkasaigak <alka.saigak@yandex.ru>
2019-10-28 10:01:13 +0300

    I'm pluha

--- cinemaparce/__init__.py
+++ cinemaparce/__init__.py
@@ -2,3 +2,6 @@
 class CinemaParcer(object):
     def __init__(city = "msk"):
         self.city = city
+    def extract_raw_content():
+        self.content = requests.get('https://' + self.city + ".subcity.ru").text
+
