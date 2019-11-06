b9555d6f43899ec3220515ffc71cf4d54f427a40
alkasaigak <alka.saigak@yandex.ru>
2019-11-06 13:53:25 +0300

    Pluha

--- cinemaparce/__init__.py
+++ cinemaparce/__init__.py
@@ -1,16 +1,53 @@
-#GO
 import requests
-'''Helps with requests'''
-class CinemaParcer(object):
-    '''Parces cinema sites'''
-    def __init__(self, city = "msk"):
+from bs4 import BeautifulSoup
+'''Helpfull moduls'''
+
+class CinemaParser:
+    '''Class. Parces cinema sites'''
+    def __init__(self, city='msk'):
         '''Initing class'''
         self.city = city
-        self.content = ""
+        self.content = None
+
     def extract_raw_content(self):
         '''Extracting raw content'''
-        self.content = requests.get('https://' + self.city + ".subcity.ru").text
+        self.content = requests.get('https://' + self.city + '.subscity.ru')
+
     def print_raw_content(self):
         '''Printing raw content'''
-        print(self.content)
+        print(self.content.text)
 
+    def get_film_list(self):
+        '''Finding actual films'''
+        text = BeautifulSoup(self.content.text, features='lxml')
+        unparsed_films = text.find_all('div', {'class': 'movie-title'})
+        ans = []
+        for film in unparsed_films:
+            try:
+                film.text.replace('\xad', '')
+                film.text.replace('\xa0', '')
+                ans.append(film.text.split(' (')[0])
+            except:
+                pass
+        return ans
+    
+    def get_film_page(self, name):
+        '''Finding a page of the film'''
+        film_items = BeautifulSoup(self.content.text, features='lxml').find_all('div', {'class': 'movie-plate', 'attr-title': name})
+        for item in film_items:
+            ans = str(item)[str(item).find('http://m.kinopoisk.ru/movie/'):].split('"')[0]
+            if (len(ans) != 0):
+                return ans;
+        return None
+
+    def get_film_nearest_session(self, name):
+        '''Finding film nearest session'''
+        page = self.get_film_page(name)
+        content = requests.get(page)
+        link = (page + content.text[content.text.find('afisha/city/'):].split('"')[0])
+        content = requests.get(link)
+        print(link)
+        print(content.text)
+        cinema = content.text[content.text.find("class= ' '") + 10:].split('<')[0]
+        return cinema
+        
